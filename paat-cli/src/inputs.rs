b02c85282260ec97df98b69539ed862e244cafb2
Kaarel Allemann <kaarelallemann@gmail.com>
2021-07-31 12:26:54 +0300

    sort events

---
+++ paat-cli/src/inputs.rs
@@ -0,0 +1,15 @@
+use paat_core::datetime::get_naive_date;
+use dialoguer::{Input};
+use chrono::{NaiveDate, ParseResult};
+
+
+pub fn input_departure_date() -> ParseResult<NaiveDate> {
+  let date_input : String = Input::new()
+    .with_prompt("Please enter the date to watch")
+    .default("2021-07-30".into())
+    .interact_text().unwrap();
+  let departure_date = get_naive_date(&date_input)?;
+
+  println!("Departure date: {:?}", departure_date);
+  Ok(departure_date)
+}
--- paat-cli/src/main.rs
+++ paat-cli/src/main.rs
@@ -1,23 +1,22 @@
+mod inputs;
+
 use actix::Actor;
-use paat_core::{actors::event::{EventManager, WaitForSpot, FetchEvents}, datetime::{get_naive_date}};
-use dialoguer::{Input, Select};
+use paat_core::{actors::event::{EventManager, WaitForSpot, FetchEvents}};
+use dialoguer::{Select, theme::ColorfulTheme};
+
+use crate::inputs::input_departure_date;
 
 #[actix::main]
 async fn main() {
-  let date_input : String = Input::new()
-    .with_prompt("Please enter the date to watch")
-    .default("2021-07-30".into())
-    .interact_text().unwrap();
-  let departure_date = get_naive_date(&date_input).unwrap();
-  println!("Departure date: {:?}", departure_date);
-
+  let departure_date = input_departure_date().unwrap();
   let address = EventManager::new(departure_date).start();
 
   match address.send(FetchEvents).await {
-    Ok(Ok(Some(events))) => {
-      let selection: usize = Select::new()
-      .items(&events)
-      .interact().unwrap();
+    Ok(Ok(Some(mut events))) => {
+       events.sort_by_key(|event| event.start.clone());
+      let selection: usize = Select::with_theme(&ColorfulTheme::default())
+        .items(&events)
+        .interact().unwrap();
       let selected_event = &events[selection];
       address.send(WaitForSpot(selected_event.uuid.clone())).await.unwrap().unwrap();
     }
